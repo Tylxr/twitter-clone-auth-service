aacceac7bdd658afa8123bf22e53b131704dde99
Tyler Marshall <tylerjeremiahh@gmail.com>
2023-10-14 14:42:36 +0100

    Preparing register service

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -2,6 +2,11 @@ import { Request, Response, NextFunction } from "express";
 
 export function register(req: Request, res: Response, next: NextFunction) {
 	// call service
+	try {
+	} catch (err) {
+		console.error(err);
+		res.sendStatus(500);
+	}
 }
 export function login(req: Request, res: Response, next: NextFunction) {
 	// call service
--- src/services/index.ts
+++ src/services/index.ts
@@ -1,4 +1,7 @@
-export function register() {}
+export function register(username: string, password: string) {
+    
+}
+
 export function login() {}
 export function isAuthenticated() {}
 
